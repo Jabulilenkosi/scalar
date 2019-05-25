7198b815227ca93780f67c59feebf9c08ca5630f
Jabulile Nkosi <jabulile.nkosi@fnb.co.za>
2019-05-25 09:25:22 +0200

    Initial commit

---
+++ src/main/java/za/co/scalar/scalar/ScalarApplication.java
@@ -0,0 +1,13 @@
+package za.co.scalar.scalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/scalar/scalar/ScalarApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.scalar.scalar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScalarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
